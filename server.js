1d2eb19f2c7b52a44eb60fc5370265a5ecaa6feb
Tomoyuki Hagiwara <hagiwara.t.tam@tam-tam.co.jp>
2018-12-30 16:27:27 +0900

    Initial commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const fs = require('fs')
+const hbs = require('hbs')
+
+var app = express()
+
+
+// hbs 設定
+app.set('view engine', 'hbs')
+hbs.registerPartials(__dirname + '/views/partials') // hbs include指定
+hbs.registerHelper('getCurrentYear', () => { // hbs helper関数
+  return new Date().getFullYear()
+})
+
+// // メンテナンスモード
+// // アクセスしてもメンテナンス画面が表示, next()を実施していないので、次の処理に行かない
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs')
+// })
+
+// 静的配信
+app.use(express.static(__dirname + '/public'))
+
+// ルーター
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'HOME page',
+    welcomeMsg: 'welcome to my home page'
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+  })
+})
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMsg: 'unable to handle request'
+  })
+})
+
+// logger
+app.use((req, res, next) => {
+  var now = new Date().toString()
+  var log = `${now}: ${req.method} ${req.url}`
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('unable to append to server.log')
+    }
+  })
+  next()
+})
+
+app.listen(3000, () => {
+  console.log('server starting ......')
+})
